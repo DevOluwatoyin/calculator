976aad389b82a080822a1d7bf2a1050e24bfee94
Oluwatoyin <oluwatoyinjolaoso001@gmail>
2023-12-12 15:56:14 +0100

    competed basic calcultion operations

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ const questionDisplay = document.getElementById("display");
 const answerDisplay = document.getElementById("result");
 const digits = document.querySelectorAll(".digit");
 const equalTo = document.getElementById("equalto");
+const del = document.getElementById("delete");
+const reset = document.getElementById("reset");
 
 for (const digit of digits) {
   digit.addEventListener("click", function (e) {
     questionDisplay.value += e.target.value;
-    // console.log(e.target.value);
   });
 }
 
-equalTo.addEventListener("click", function (e) {
-  try {
-    let answer = eval(questionDisplay.value);
-    answerDisplay.value = answer;
-    console.log(eval(questionDisplay.value));
-    questionDisplay.value = "";
-  } catch (error) {
-    answerDisplay.value = "syntax error";
-    console.log(error);
-  }
+equalTo.addEventListener("click", function () {
+  let answer = eval(questionDisplay.value);
+  answerDisplay.value = answer;
+  questionDisplay.value = "";
+});
+
+del.addEventListener("click", function () {
+  questionDisplay.value = questionDisplay.value.slice(0, -1);
+});
+
+reset.addEventListener("click", function () {
+  questionDisplay.value = "";
+  answerDisplay.value = "";
 });
